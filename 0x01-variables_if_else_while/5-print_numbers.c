8417857b791277b6aeedfa11dfcc03a76215379c
Mephic <kmephic@gmail.com>
2022-07-03 11:15:28 -0700

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main-will print small letters
+*for loop in ASCII
+*
+* alp is an integer
+* Return: always 0
+*/
+
+int main(void)
+{
+int m;
+for (m = 0; m < 10; m++)
+{
+	printf("%d\n, m");
+}
+return (0);
+}
+
+
