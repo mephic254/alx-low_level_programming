434cf4c61fcc3bb80426d1017a87be2da9ddef60
Mephic <kmephic@gmail.com>
2022-07-03 11:45:25 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main-will print small letters
+*for loop in ASCII
+*
+* alp is an integer
+* Return: always 0
+*/
+
+int main(void)
+{
+int m;
+for (m = 48; m < 57; m++)
+{
+	putchar(m);
+}
+putchar(10);
+return (0);
+}
+
+
