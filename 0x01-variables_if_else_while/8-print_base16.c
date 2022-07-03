2e13413b8970fec4b8c787bd3d99ec5a9633c129
Mephic <kmephic@gmail.com>
2022-07-03 12:34:25 -0700

    task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main-will print small letters
+*for loop in ASCII
+*
+* alp is an integer
+* Return: always 0
+*/
+
+int main(void)
+{
+int m;
+int n;
+for (m = 48; m <= 57; m++)
+{
+	putchar(m);
+}
+for (n = 97; m <= 102; n++)
+{
+	putchar(n);
+}
+putchar(10);
+return (0);
+}
+
+
