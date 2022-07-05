37107df79625975506c358aeeb0dbf212aae7ac4
Mephic <kmephic@gmail.com>
2022-07-05 15:17:01 -0700

    t4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower -> checks value of character if lowercase
+ * @c: character value
+ * Return: 1 if lowercase 0 if false
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
