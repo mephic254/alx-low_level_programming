03c175d0e27eedccca91fb2d1e5b4c828ef73bd6
Mephic <kmephic@gmail.com>
2022-07-05 16:10:02 -0700

    t7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - gives the absolute value of a number
+ * @n: integer
+ * Return: value of absolute number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	n = -n;
+	return (n);
+}
+
